fdf0d76b7f653ac06fdfa297420f1d13a8855ae9
EchoeMatrix <100523018+EchoeMatrix@users.noreply.github.com>
2026-04-04 13:27:45 +0530

    update 1

--- webapp/src/firebase.js
+++ webapp/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAk6dJKBDOBFFwsnLphgLJoxZhpmVZXVJE",
-  authDomain: "inertia-run-club-5cd85.firebaseapp.com",
-  projectId: "inertia-run-club-5cd85",
-  storageBucket: "inertia-run-club-5cd85.firebasestorage.app",
-  messagingSenderId: "73961410218",
-  appId: "1:73961410218:web:20609eccecc01a50c64ad3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
